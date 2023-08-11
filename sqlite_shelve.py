66e42c28fefd946f048e736234d5c6e7d266255c
kelvin <kfkelvinng@gmail.com>
2023-08-11 18:28:11 +0100

    sqlite_store sync

--- sqlite_shelve.py
+++ sqlite_shelve.py
@@ -4,6 +4,7 @@ import gzip
 
 import sqlite3
 
+
 class Connection:
     def __init__(self, path_str=":memory:", mode="rw"):
         if "w" not in mode:
@@ -19,6 +20,9 @@ class Connection:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
+    def __del__(self):
+        self.close()
+
     def close(self):
         self.flush()
         self._conn.close()
@@ -28,7 +32,7 @@ class Connection:
 
     def shelve(self, shelve_name: str = None, mode="rw") -> "SqliteShelve":
         if "w" not in mode:
-            assert shelve_name in map(lambda t:t[1], self.sqlite_schema())
+            assert shelve_name in map(lambda t: t[1], self.sqlite_schema())
         return SqliteShelve(self._conn, table_name=shelve_name)
 
     def sqlite_schema(self):
@@ -49,6 +53,12 @@ class SqliteShelve:
         self._conn.execute(
             f"CREATE TABLE IF NOT EXISTS {self.table_name} (NAME TEXT PRIMARY KEY ON CONFLICT REPLACE, DATA BLOB)")
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._conn.flush()
+
     def __getitem__(self, item):
         name, blb = self._conn.execute(f"SELECT NAME, DATA FROM {self.table_name} WHERE NAME=?", [item]).fetchone()
         return self._decompress(blb)
@@ -60,7 +70,8 @@ class SqliteShelve:
         return pickle.loads(bz2.decompress(blb))
 
     def __setitem__(self, key, value):
-        self._conn.execute(f"INSERT INTO {self.table_name} (NAME, DATA) VALUES (?,?)", [key, gzip.compress(pickle.dumps(value))])
+        self._conn.execute(f"INSERT INTO {self.table_name} (NAME, DATA) VALUES (?,?)",
+                           [key, gzip.compress(pickle.dumps(value))])
 
     def __delitem__(self, key, value):
         self._conn.execute(f"DELETE {self.table_name} where NAME=(?)", [key, gzip.compress(pickle.dumps(value))])
@@ -80,4 +91,5 @@ class SqliteShelve:
         return map(lambda x: self._decompress(x[1]), self._conn.execute(f"SELECT NAME, DATA FROM {self.table_name}"))
 
     def items(self):
-        return map(lambda x: (x[0], self._decompress(x[1])), self._conn.execute(f"SELECT NAME, DATA FROM {self.table_name}"))
+        return map(lambda x: (x[0], self._decompress(x[1])),
+                   self._conn.execute(f"SELECT NAME, DATA FROM {self.table_name}"))
